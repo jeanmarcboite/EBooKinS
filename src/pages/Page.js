f8dc31000a42aadfedb95d2b5bdfc70fb76d07f2
jmboite <jeanmarc.boite@gmail.com>
2020-04-06 15:06:02 +0200

    Page OK

--- src/pages/Page.js
+++ src/pages/Page.js
@@ -1,15 +1,23 @@
 import React from "react";
 import { connect } from "react-redux";
 import { MenuProvider, Menu, theme, animation } from "react-contexify";
+import "react-contexify/dist/ReactContexify.min.css";
+
 function Page({ menu, children, settings }) {
   return (
     <>
-      <Menu id="menu_id">{menu}</Menu>
+      <Menu
+        id="menu_id"
+        theme={settings.darkMode ? theme.dark : theme.light}
+        animation={animation.flip}
+      >
+        {menu}
+      </Menu>
       <MenuProvider
         id="menu_id"
         style={{
           border: "1px solid purple",
-          display: "flex",
+          height: "100vh",
         }}
       >
         {children}
--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -5,39 +5,49 @@ import { Form, Switch, Select } from "antd";
 import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
 import Page from "pages/Page";
 import RoutesMenu from "routes/Menu";
+
+const divStyle = {
+  width: "100vw",
+  height: "100vh",
+};
+
 class Settings extends React.Component {
   render() {
     return (
-      <Page menu={<RoutesMenu />}>
-        <Form>
-          <Form.Item label="Dark Mode">
-            <Switch
-              checkedChildren={<CheckOutlined />}
-              unCheckedChildren={<CloseOutlined />}
-              checked={this.props.settings.darkMode}
-              onChange={(value) =>
-                this.props.dispatch(setSetting({ setting: "darkMode", value }))
-              }
-            ></Switch>
-          </Form.Item>
-          <Form.Item label="Menu trigger">
-            <Select
-              defaultValue={this.props.settings.menuTrigger}
-              style={{ width: 200 }}
-              onChange={(value) =>
-                this.props.dispatch(
-                  setSetting({ setting: "menuTrigger", value })
-                )
-              }
-            >
-              <Select.Option value="context">Right click</Select.Option>
-              <Select.Option value="double">Double Left click</Select.Option>
-              <Select.Option value="disabled" disabled>
-                Disabled
-              </Select.Option>
-            </Select>
-          </Form.Item>
-        </Form>
+      <Page menu={<RoutesMenu />} style={{ divStyle }}>
+        <div style={{ divStyle }}>
+          <Form style={{ divStyle }}>
+            <Form.Item label="Dark Mode">
+              <Switch
+                checkedChildren={<CheckOutlined />}
+                unCheckedChildren={<CloseOutlined />}
+                checked={this.props.settings.darkMode}
+                onChange={(value) =>
+                  this.props.dispatch(
+                    setSetting({ setting: "darkMode", value })
+                  )
+                }
+              ></Switch>
+            </Form.Item>
+            <Form.Item label="Menu trigger">
+              <Select
+                defaultValue={this.props.settings.menuTrigger}
+                style={{ width: 200 }}
+                onChange={(value) =>
+                  this.props.dispatch(
+                    setSetting({ setting: "menuTrigger", value })
+                  )
+                }
+              >
+                <Select.Option value="context">Right click</Select.Option>
+                <Select.Option value="double">Double Left click</Select.Option>
+                <Select.Option value="disabled" disabled>
+                  Disabled
+                </Select.Option>
+              </Select>
+            </Form.Item>
+          </Form>
+        </div>
       </Page>
     );
   }

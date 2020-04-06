86853faa7409d1d0de7d83b6b121ba30cb7752f8
jmboite <jeanmarc.boite@gmail.com>
2020-04-06 12:14:40 +0200

    error 404

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Settings from "pages/Settings";
-
+import Error404 from "pages/Error404";
 export default function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/" component={Settings} />
         <Route path="/settings" component={Settings} />
+        <Route component={Error404} />
       </Switch>
     </Router>
   );

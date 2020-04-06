396fc3dfddc571281ae091d2a40e7cf3385aecc7
jmboite <jeanmarc.boite@gmail.com>
2020-04-06 08:08:33 +0200

    switch in Form

--- src/pages/Settings/index.js
+++ src/pages/Settings/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import { connect } from "react-redux";
 import { setDarkMode } from "./store";
-import { Switch } from "antd";
+import { Form, Switch } from "antd";
 import { CloseOutlined, CheckOutlined } from "@ant-design/icons";
 
 class Settings extends React.Component {
   render() {
     return (
-      <Switch
-        checkedChildren={<CheckOutlined />}
-        unCheckedChildren={<CloseOutlined />}
-        checked={this.props.settings.darkMode}
-        onChange={(checked) => this.props.dispatch(setDarkMode(checked))}
-      />
+      <Form>
+        <Form.Item label="Dark Mode">
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            checked={this.props.settings.darkMode}
+            onChange={(checked) => this.props.dispatch(setDarkMode(checked))}
+          ></Switch>
+        </Form.Item>
+      </Form>
     );
   }
 }

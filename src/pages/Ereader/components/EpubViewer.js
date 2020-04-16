822a840d64fcb21ebbff8e10d5431c829bd5427e
jmboite <jeanmarc.boite@gmail.com>
2020-04-16 15:41:28 +0200

    load book

--- src/pages/Ereader/components/EpubViewer.js
+++ src/pages/Ereader/components/EpubViewer.js
@@ -94,7 +94,7 @@ class EpubViewer extends React.PureComponent {
       this.state.url !== this.props.url ||
       this.leftPanelSize !== this.props.settings.leftPanelSize
     ) {
-      //this.setState({ url: this.props.url, error: null });
+      this.setState({ url: this.props.url, error: null });
       this.loadBook();
     } else if (
       false &&

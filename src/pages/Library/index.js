4ceb3afba4c8b120990364d86b0616d91ff5cfde
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-04-23 09:56:04 +0200

    add cards

--- src/pages/Library/index.js
+++ src/pages/Library/index.js
@@ -3,11 +3,54 @@ import { connect } from "react-redux";
 import Page from "pages/Page";
 import RoutesMenu from "routes/Menu";
 import { ThemeContext } from "ThemeProvider";
+import { Card, Avatar } from "antd";
+import {
+  SettingOutlined,
+  EditOutlined,
+  EllipsisOutlined,
+} from "@ant-design/icons";
 
 class Library extends React.Component {
   static contextType = ThemeContext;
+
   render() {
-    return <Page menu={<RoutesMenu />}>Library</Page>;
+    const gridStyle = {
+      width: "25%",
+      textAlign: "center",
+    };
+    return (
+      <Page menu={<RoutesMenu />}>
+        <Card
+          style={{ width: 300 }}
+          cover={
+            <img
+              alt="example"
+              src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"
+            />
+          }
+          actions={[
+            <SettingOutlined key="setting" />,
+            <EditOutlined key="edit" />,
+            <EllipsisOutlined key="ellipsis" />,
+          ]}
+        >
+          <Card.Meta
+            avatar={
+              <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+            }
+            title="With avatar"
+            description="This is the description"
+          />
+        </Card>
+        <Card title="Card Title">
+          <Card.Grid style={gridStyle}>Default</Card.Grid>
+          <Card.Grid hoverable={false} style={gridStyle}>
+            not hoverable
+          </Card.Grid>
+        </Card>
+        ,
+      </Page>
+    );
   }
 }
 

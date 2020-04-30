e84320eae51e0ca0273b3cd87cc8d061da989b44
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-04-30 14:35:44 +0200

    description from ebook

---
+++ src/pages/Book/components/BookDetails.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { ThemeContext } from "ThemeProvider";
+import Tags from "components/Tags";
+import style from "./Book.module.css";
+import Rating from "react-rating";
+import DB from "lib/Database";
+import Author from "models/Author";
+import { StarOutlined, StarFilled } from "@ant-design/icons";
+import renderHTML from "react-render-html";
+
+export default class BookDetails extends React.Component {
+  static contextType = ThemeContext;
+
+  constructor(props) {
+    super(props);
+    console.log(this.props);
+    this.state = {
+      book: {},
+      author: { img: "", name: "" },
+    };
+  }
+
+  componentDidMount() {
+    if (this.props.isbn) {
+      DB.ebooks.db.get(this.props.isbn).then((book) => {
+        this.setState({ book: book });
+        if (book.author) {
+          let author = new Author(book.author);
+          author
+            .get()
+            .then((author) => this.setState({ author }))
+            .catch(console.warn);
+        }
+      });
+    }
+  }
+  rating = (work) => {
+    if (!work) return 0;
+    let rating = parseFloat(work.ratings_sum) / parseFloat(work.ratings_count);
+    return rating;
+  };
+  get = (work, what) => {
+    if (!work || !work[what]) return "";
+    return work[what];
+  };
+
+  getOriginalPublicationDate = () => {
+    let work = this.props.data.work;
+    if (!work || !work.original_publication_year) return "";
+    if (
+      work.original_publication_day === 1 &&
+      work.original_publication_month === 1
+    )
+      return work.original_publication_year;
+    return `${work.original_publication_day}/${work.original_publication_month}/${work.original_publication_year}`;
+  };
+
+  render = () => {
+    console.log("DETAILSprops", this.props);
+    return (
+      <div className={style.container}>
+        <div className={style.cover}>
+          <img
+            src={this.props.image_url}
+            alt="cover"
+            width="100%"
+            onClick={this.props.onRead}
+          />
+          <div>{this.getOriginalPublicationDate()}</div>
+        </div>
+        <div className={style.rating}>
+          <Rating
+            readonly
+            initialRating={this.rating(this.props.data.work)}
+            emptySymbol={<StarOutlined />}
+            fullSymbol={<StarFilled />}
+          />
+          <div>{this.get(this.props.data.work, "ratings_count")} ratings</div>
+          <div>{this.get(this.props.data.work, "reviews_count")} reviews</div>
+        </div>
+        <div className={style.title}>{this.props.data.title}</div>
+        <a className={style.links} href={this.get(this.props.data, "url")}>
+          <img
+            alt="goodreads"
+            src="http://d.gr-assets.com/misc/1454549125-1454549125_goodreads_misc.png"
+          />
+        </a>
+        <div className={style.author}>
+          <img
+            className={style.author_img}
+            src={this.state.author.img}
+            alt={this.state.author.name}
+            height="80"
+          />
+          <h2>{this.state.author.name} </h2>
+        </div>
+        <div className={style.description}>
+          {this.state.book.description
+            ? renderHTML(this.state.book.description)
+            : this.props.data.description}
+        </div>
+        <div className={style.shelves}>
+          <Tags subject={this.state.book.subject}></Tags>
+        </div>
+      </div>
+    );
+  };
+}
--- src/pages/Book/index.js
+++ src/pages/Book/index.js
@@ -5,17 +5,14 @@ import { ThemeContext } from "ThemeProvider";
 import MainLayout from "pages/MainLayout";
 import Book from "models/Book";
 import renderHTML from "react-render-html";
-import Rating from "react-rating";
 // import the react-json-view component
 import ReactJson from "react-json-view";
-import style from "./Book.module.css";
 import { loadFile } from "pages/Read/store";
 import { withRouter } from "react-router-dom";
 import Author from "models/Author";
 import DB from "lib/Database";
-import Tags from "components/Tags";
 
-import { StarOutlined, StarFilled } from "@ant-design/icons";
+import BookDetails from "./components/BookDetails";
 class BookPage extends React.Component {
   static contextType = ThemeContext;
 
@@ -23,31 +20,19 @@ class BookPage extends React.Component {
     super(props);
 
     this.state = {
-      isbn: this.props.match.params.isbn,
       image_url: "",
       book: { data: { description: "" } },
-      db_book: {},
       author: { img: "", name: "" },
     };
   }
   componentDidMount() {
     let ISBN = this.props.match.params.isbn;
     let book = new Book(ISBN);
-    DB.ebooks.db.get(ISBN).then((book) => {
-      this.setState({ db_book: book });
-      if (book.author) {
-        let author = new Author(book.author);
-        author
-          .get()
-          .then((author) => this.setState({ author }))
-          .catch(console.warn);
-      }
-    });
     book
       .getFromISBN()
       .then((book) => {
         let image_url = book.data.image_url;
-        this.setState({ book, image_url });
+        this.setState({ book: book, image_url });
       })
       .catch(console.warn);
   }
@@ -63,71 +48,15 @@ class BookPage extends React.Component {
     this.props.dispatch(loadFile(this.state.isbn));
     this.props.history.push("/");
   };
-
-  rating = (work) => {
-    if (!work) return 0;
-    let rating = parseFloat(work.ratings_sum) / parseFloat(work.ratings_count);
-    return rating;
-  };
-  get = (work, what) => {
-    if (!work || !work[what]) return "";
-    return work[what];
-  };
-  getOriginalPublicationDate = () => {
-    let work = this.state.book.data.work;
-    if (!work || !work.original_publication_year) return "";
-    if (
-      work.original_publication_day === 1 &&
-      work.original_publication_month === 1
-    )
-      return work.original_publication_year;
-    return `${work.original_publication_day}/${work.original_publication_month}/${work.original_publication_year}`;
-  };
   render = () => {
-    let data = this.state.book.data;
+    console.log("RENDER", this.state);
     return (
       <MainLayout>
-        <div className={style.container}>
-          <div className={style.cover}>
-            <img
-              src={this.state.image_url}
-              alt="cover"
-              width="100%"
-              onClick={this.onRead}
-            />
-            <div>{this.getOriginalPublicationDate()}</div>
-          </div>
-          <div className={style.rating}>
-            <Rating
-              readonly
-              initialRating={this.rating(data.work)}
-              emptySymbol={<StarOutlined />}
-              fullSymbol={<StarFilled />}
-            />
-            <div>{this.get(data.work, "ratings_count")} ratings</div>
-            <div>{this.get(data.work, "reviews_count")} reviews</div>
-          </div>
-          <div className={style.title}>{data.title}</div>
-          <a className={style.links} href={this.get(data, "url")}>
-            <img
-              alt="goodreads"
-              src="http://d.gr-assets.com/misc/1454549125-1454549125_goodreads_misc.png"
-            />
-          </a>
-          <div className={style.author}>
-            <img
-              className={style.author_img}
-              src={this.state.author.img}
-              alt={this.state.author.name}
-              height="80"
-            />
-            <h2>{this.state.author.name} </h2>
-          </div>
-          <div className={style.description}>{this.description(false)}</div>
-          <div className={style.shelves}>
-            <Tags subject={this.state.db_book.subject}></Tags>
-          </div>
-        </div>
+        <BookDetails
+          image_url={this.state.image_url}
+          isbn={this.props.match.params.isbn}
+          data={this.state.book.data}
+        />
       </MainLayout>
     );
   };

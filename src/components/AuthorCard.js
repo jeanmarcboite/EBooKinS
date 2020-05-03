1c66f8c63b6f904557dfa647899015f8a5b6aaf1
jeanmarcboite <jeanmarc.boite@gmail.com>
2020-05-03 13:31:43 +0200

    renderhtml

--- src/components/AuthorCard.js
+++ src/components/AuthorCard.js
@@ -1,15 +1,25 @@
 import React from "react";
+import renderHTML from "react-render-html";
 
+const image_url = (author) => {
+  return author.data?.large_image_url || author.image_url;
+};
 export default class AuthorCard extends React.Component {
   render() {
     let authors = this.props.author;
     if (!Array.isArray(authors)) authors = [authors];
-
+    console.log(authors);
     return authors.map((author, key) => (
       <div key={key}>
-        <img src={author.image_url} alt={author.name} height="80" />
+        <img
+          src={image_url(author)}
+          alt={author.name}
+          height="80"
+          style={{ float: "right" }}
+        />
         <h2>{author.name} </h2>
         {author.role}
+        {renderHTML(author.data?.about[0] || "")}
       </div>
     ));
   }
